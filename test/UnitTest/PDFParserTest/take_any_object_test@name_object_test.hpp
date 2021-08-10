b78397248381da1b4b4f2962979314c8f47e7290
KodaiYM <solceansky@gmail.com>
2021-08-11 01:46:03 +0900

    take_any_object_test の name_object_test の実装

--- test/UnitTest/PDFParserTest/take_any_object_test@name_object_test.hpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@name_object_test.hpp
@@ -5,7 +5,7 @@
 namespace pdfparser_test { namespace take_any_object_test {
 	[TestClass] public ref class name_object_test {
 	public:
-		[TestMethod] void test_name_containing_slash();
+		[TestMethod] void test_valid_name();
 		[TestMethod] void test_empty_name();
 		[TestMethod] void test_hexadecimal_code();
 		[TestMethod] void test_invalid_hexadecimal_code();
