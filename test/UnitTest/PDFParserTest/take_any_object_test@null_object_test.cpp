45c2b3a769a4da2e274654384a3054d1223c7340
KodaiYM <solceansky@gmail.com>
2021-08-12 02:13:24 +0900

    take_any_object_test の null_object_test の実装

--- test/UnitTest/PDFParserTest/take_any_object_test@null_object_test.cpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@null_object_test.cpp
@@ -10,4 +10,14 @@ using namespace pdfparser_test;
 
 using namespace take_any_object_test;
 
-// TODO: ŽÀ‘•
+void null_object_test::test_null() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "null";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(null == std::get<null_object>(object));
+}
--- test/UnitTest/PDFParserTest/take_any_object_test@null_object_test.hpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@null_object_test.hpp
@@ -5,6 +5,6 @@
 namespace pdfparser_test { namespace take_any_object_test {
 	[TestClass] public ref class null_object_test {
 	public:
-		// Null Object
+		[TestMethod] void test_null();
 	};
 }} // namespace pdfparser_test::take_any_object_test
