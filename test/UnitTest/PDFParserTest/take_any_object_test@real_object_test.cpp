50b51b2cdfb597e755192ac643394fe7534d48d2
KodaiYM <solceansky@gmail.com>
2021-08-12 03:09:44 +0900

    take_any_object_test の real_object_test の実装

--- test/UnitTest/PDFParserTest/take_any_object_test@real_object_test.cpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@real_object_test.cpp
@@ -10,4 +10,102 @@ using namespace pdfparser_test;
 
 using namespace take_any_object_test;
 
-// TODO: À‘•
+void real_object_test::test_unsigned_leading_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << ".0123";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(0.0123 == std::get<real_object>(object));
+}
+void real_object_test::test_unsigned_trailing_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "0123.";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(123.0 == std::get<real_object>(object));
+}
+void real_object_test::test_unsigned_embedded_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "34.5";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(34.5 == std::get<real_object>(object));
+}
+void real_object_test::test_plus_leading_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "+.002";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(0.002 == std::get<real_object>(object));
+}
+void real_object_test::test_plus_trailing_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "+0123.";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(123.0 == std::get<real_object>(object));
+}
+void real_object_test::test_plus_embedded_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "+0123.6";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(123.6 == std::get<real_object>(object));
+}
+void real_object_test::test_minus_leading_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "-.002";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(-0.002 == std::get<real_object>(object));
+}
+void real_object_test::test_minus_trailing_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "-04.";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(-4.0 == std::get<real_object>(object));
+}
+void real_object_test::test_minus_embedded_period() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "-3.62";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(-3.62 == std::get<real_object>(object));
+}
