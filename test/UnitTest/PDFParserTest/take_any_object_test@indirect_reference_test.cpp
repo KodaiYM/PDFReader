7a7476449a16f1a0a98c7a080217b804639dbd73
KodaiYM <solceansky@gmail.com>
2021-08-12 12:00:10 +0900

    take_any_object_test の indirect_reference_test の実装

--- test/UnitTest/PDFParserTest/take_any_object_test@indirect_reference_test.cpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@indirect_reference_test.cpp
@@ -10,4 +10,27 @@ using namespace pdfparser_test;
 
 using namespace take_any_object_test;
 
-// TODO: À‘•
+void indirect_reference_test::test_unsigned() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "3 8 R";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(indirect_reference{3, 8} ==
+	               std::get<indirect_reference>(object));
+}
+void indirect_reference_test::test_signed() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "+3 +8 R";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(indirect_reference{3, 8} ==
+	               std::get<indirect_reference>(object));
+}
--- test/UnitTest/PDFParserTest/take_any_object_test@indirect_reference_test.hpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@indirect_reference_test.hpp
@@ -5,6 +5,7 @@
 namespace pdfparser_test { namespace take_any_object_test {
 	[TestClass] public ref class indirect_reference_test {
 	public:
-		// Indirect Reference
+		[TestMethod] void test_unsigned();
+		[TestMethod] void test_signed();
 	};
 }} // namespace pdfparser_test::take_any_object_test
