67619b64aa2f61105e7767eedabbe8d7f42f3e2d
KodaiYM <solceansky@gmail.com>
2021-08-12 12:33:40 +0900

    take_any_object の unknown_test の実装

---
+++ test/UnitTest/PDFParserTest/take_any_object_test@unknown_test.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "testtool.h"
+
+namespace pdfparser_test { namespace take_any_object_test {
+	[TestClass] public ref class unknown_test {
+	public:
+		[TestMethod] void test_unknown();
+	};
+}} // namespace pdfparser_test::take_any_object_test
