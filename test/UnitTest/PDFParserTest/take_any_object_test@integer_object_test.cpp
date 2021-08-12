0ea697847c633aa003c732375a791cd045d2b06e
KodaiYM <solceansky@gmail.com>
2021-08-12 11:57:00 +0900

    take_any_object_test の integer_object_test の実装

--- test/UnitTest/PDFParserTest/take_any_object_test@integer_object_test.cpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@integer_object_test.cpp
@@ -10,4 +10,36 @@ using namespace pdfparser_test;
 
 using namespace take_any_object_test;
 
-// TODO: À‘•
+void integer_object_test::test_unsigned_integer() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "0123";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(123 == std::get<integer_object>(object));
+}
+void integer_object_test::test_plus_integer() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "+0123";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(123 == std::get<integer_object>(object));
+}
+void integer_object_test::test_minus_integer() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "-0123";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto          object = str_parser.take_any_object(obj_pool);
+	Assert::IsTrue(-123 == std::get<integer_object>(object));
+}
--- test/UnitTest/PDFParserTest/take_any_object_test@integer_object_test.hpp
+++ test/UnitTest/PDFParserTest/take_any_object_test@integer_object_test.hpp
@@ -5,6 +5,8 @@
 namespace pdfparser_test { namespace take_any_object_test {
 	[TestClass] public ref class integer_object_test {
 	public:
-		// Integer Object
+		[TestMethod] void test_unsigned_integer();
+		[TestMethod] void test_plus_integer();
+		[TestMethod] void test_minus_integer();
 	};
 }} // namespace pdfparser_test::take_any_object_test
