1d3540595ef52ce04eb5232055c9eb2888636906
KodaiYM <solceansky@gmail.com>
2021-08-15 21:47:23 +0900

    設計書の修正に合わせて、テストケースの修正

--- test/UnitTest/PDFParserTest/take_footer_test.hpp
+++ test/UnitTest/PDFParserTest/take_footer_test.hpp
@@ -3,31 +3,24 @@
 #include "testtool.h"
 
 namespace pdfparser_test {
-[TestClass] public ref class require_test {
+[TestClass] public ref class take_footer_test {
 public:
+	[TestMethod] void test_sample();
+
 	[TestMethod] void test_EOF_EOF_EOL();
 	[TestMethod] void test_EOF_EOF_only();
 	[TestMethod] void test_EOF_EOF_not_EOL();
 	[TestMethod] void test_EOF_not_EOF();
 	[TestMethod] void test_EOF_SP_EOF();
-	[TestMethod] void test_EOF_end_of_line();
 
-	[TestMethod] void test_startxref_startxref_EOL();
+	[TestMethod] void test_xref_byte_offset_not_found();
+
 	[TestMethod] void test_startxref_SP_startxref_comment_EOL();
-	[TestMethod] void test_startxref_startxref_only();
 	[TestMethod] void test_startxref_startxref_not_EOL();
 	[TestMethod] void test_startxref_not_startxref();
-	[TestMethod] void test_startxref_end_of_line();
 
-	[TestMethod] void test_xref_xref_EOL();
 	[TestMethod] void test_xref_SP_xref_comment_EOL();
-	[TestMethod] void test_xref_xref_only();
 	[TestMethod] void test_xref_xref_not_EOL();
 	[TestMethod] void test_xref_not_xref();
-	[TestMethod] void test_xref_end_of_line();
-
-	[TestMethod] void test_space_space_only();
-	[TestMethod] void test_space_not_space();
-	[TestMethod] void test_space_end_of_line();
 };
 } // namespace pdfparser_test
--- test/UnitTest/PDFParserTest/tests.cpp
+++ test/UnitTest/PDFParserTest/tests.cpp
@@ -1,5 +1,4 @@
-/*
 #include "PDFParser.error_types.cpp"
+#include "PDFParser.object_types.cpp"
 #include "PDFParser.parser.cpp"
 #include "PDFParser.xref_types.cpp"
-*/
