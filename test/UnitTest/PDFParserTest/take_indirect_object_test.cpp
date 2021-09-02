cd689000219ff47818d57858b9f9cfcd829cf906
KodaiYM <solceansky@gmail.com>
2021-09-03 01:45:35 +0900

    take_indirect_object_test の構文上の修正

--- test/UnitTest/PDFParserTest/take_indirect_object_test.cpp
+++ test/UnitTest/PDFParserTest/take_indirect_object_test.cpp
@@ -1,14 +1,13 @@
-#include "PDFParser.h"
 #include "literal_trim.hpp"
+#include "pdfparser.object_pool.hpp"
+#include "pdfparser.stream_parser.hpp"
 #include "take_indirect_object_test.hpp"
 
 #include <sstream>
 
 using namespace pdfparser;
-using namespace error_types;
 using namespace object_types;
 using namespace pdfparser_test;
-
 using namespace xref_types;
 
 void take_indirect_object_test::test_sample() {
@@ -46,7 +45,8 @@ endobj
 	object_pool   obj_pool(str_parser);
 	obj_pool.add_xref_table(
 	    xref_table{xref_inuse_entry{8, 0, offset_of_objnum8}});
-	auto object = str_parser.take_indirect_object(obj_pool, 7, 0);
+	auto object =
+	    str_parser.take_indirect_object(obj_pool, xref_inuse_entry{7, 0, 0});
 	Assert::IsTrue(
 	    stream_object{dictionary_object{{"Length", indirect_reference{8, 0}}},
 	                  stream_contents} == std::get<stream_object>(object));
@@ -64,7 +64,7 @@ endobj
 	stream_parser str_parser(std::move(stream));
 	object_pool   obj_pool(str_parser);
 	try {
-		str_parser.take_indirect_object(obj_pool, 8, 0);
+		str_parser.take_indirect_object(obj_pool, xref_inuse_entry{8, 0, 0});
 	} catch (const parse_error& parse_e) {
 		Assert::IsTrue(
 		    parse_error::indirect_object_is_inconsistent_with_xref_table ==
@@ -88,78 +88,11 @@ endobj
 	stream_parser str_parser(std::move(stream));
 	object_pool   obj_pool(str_parser);
 	try {
-		str_parser.take_indirect_object(obj_pool, 7, 1);
+		str_parser.take_indirect_object(obj_pool, xref_inuse_entry{7, 1, 0});
 	} catch (const parse_error& parse_e) {
 		Assert::IsTrue(
 		    parse_error::indirect_object_is_inconsistent_with_xref_table ==
 		    parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_indirect_object_test::test_keyword_obj_not_found() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-7 0 ob 
-	(Brillig)
-endobj
-)"_trimmed;
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-	try {
-		str_parser.take_indirect_object(obj_pool, 7, 0);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_obj_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_indirect_object_test::test_refers_indirect_reference() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-7 0 obj
-	1 0 R
-endobj
-)"_trimmed;
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-	try {
-		str_parser.take_indirect_object(obj_pool, 7, 0);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::indirect_object_refers_indirect_reference ==
-		               parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_indirect_object_test::test_keyword_endobj_not_found() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-7 0 obj
-	(Brillig)
-endob
-)"_trimmed;
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-	try {
-		str_parser.take_indirect_object(obj_pool, 7, 0);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_endobj_not_found == parse_e.code());
 
 		// success
 		return;
--- test/UnitTest/PDFParserTest/take_indirect_object_test.hpp
+++ test/UnitTest/PDFParserTest/take_indirect_object_test.hpp
@@ -8,8 +8,5 @@ public:
 	[TestMethod] void test_sample();
 	[TestMethod] void test_inconsistent_object_number();
 	[TestMethod] void test_inconsistent_generation_number();
-	[TestMethod] void test_keyword_obj_not_found();
-	[TestMethod] void test_refers_indirect_reference();
-	[TestMethod] void test_keyword_endobj_not_found();
 };
 } // namespace pdfparser_test
