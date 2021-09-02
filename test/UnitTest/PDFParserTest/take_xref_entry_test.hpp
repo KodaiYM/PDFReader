8362d0e6070c27c17aca7935d39b9de6d6252407
KodaiYM <solceansky@gmail.com>
2021-09-03 01:48:54 +0900

    take_xref_entry_test の構文上の修正

--- test/UnitTest/PDFParserTest/take_xref_entry_test.hpp
+++ test/UnitTest/PDFParserTest/take_xref_entry_test.hpp
@@ -1,12 +1,3 @@
 #pragma once
 
-#include "testtool.h"
-
-namespace pdfparser_test {
-[TestClass] public ref class take_xref_entry_test {
-public:
-	[TestMethod] void test_SPCR();
-	[TestMethod] void test_SPLF();
-	[TestMethod] void test_CRLF();
-};
-} // namespace pdfparser_test
+// doesn't test
