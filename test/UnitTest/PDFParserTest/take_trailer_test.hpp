3e7227efb9706d739d180a92da1149e3e5ab69f9
KodaiYM <solceansky@gmail.com>
2021-09-03 01:57:33 +0900

    take_trailer_test の構文上の修正

--- test/UnitTest/PDFParserTest/take_trailer_test.hpp
+++ test/UnitTest/PDFParserTest/take_trailer_test.hpp
@@ -7,8 +7,5 @@ namespace pdfparser_test {
 public:
 	[TestMethod] void test_sample_trailer();
 	[TestMethod] void test_no_white_space_after_keyword_trailer();
-	[TestMethod] void test_white_space_before_keyword_trailer();
-	[TestMethod] void test_no_dictionary_object();
-	[TestMethod] void test_no_keyword_trailer();
 };
 } // namespace pdfparser_test
