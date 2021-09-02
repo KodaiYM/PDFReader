9d5e216cb9e6b1b3434d54600684ce805671110c
KodaiYM <solceansky@gmail.com>
2021-09-02 22:57:29 +0900

    take_boolean_object の構文上の修正

---
+++ test/UnitTest/PDFParserTest/take_boolean_object_test.cpp
@@ -0,0 +1,31 @@
+#include "pdfparser.stream_parser.hpp"
+#include "take_boolean_object_test.hpp"
+
+#include <sstream>
+
+using namespace pdfparser;
+using namespace object_types;
+using namespace pdfparser_test;
+
+void take_boolean_object_test::test_true() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "true";
+
+	stream_parser str_parser(std::move(stream));
+
+	auto object = str_parser.take_boolean_object();
+	Assert::IsTrue(true == object);
+}
+void take_boolean_object_test::test_false() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "false";
+
+	stream_parser str_parser(std::move(stream));
+
+	auto object = str_parser.take_boolean_object();
+	Assert::IsTrue(false == object);
+}
---
+++ test/UnitTest/PDFParserTest/take_boolean_object_test.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "testtool.h"
+
+namespace pdfparser_test {
+[TestClass] public ref class take_boolean_object_test {
+public:
+	[TestMethod] void test_true();
+	[TestMethod] void test_false();
+};
+} // namespace pdfparser_test
