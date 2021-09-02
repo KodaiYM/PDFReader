d661902d53697ce51bda930cd9ec06e62ec3c310
KodaiYM <solceansky@gmail.com>
2021-09-02 23:10:48 +0900

    take_indirect_reference_test の構文上の修正

---
+++ test/UnitTest/PDFParserTest/take_indirect_reference_test.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "testtool.h"
+
+namespace pdfparser_test {
+[TestClass] public ref class take_indirect_reference_test {
+public:
+	[TestMethod] void test_unsigned();
+	[TestMethod] void test_signed();
+};
+} // namespace pdfparser_test
