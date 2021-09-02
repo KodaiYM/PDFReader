78199ef25075717849749c17716341d947d03ec9
KodaiYM <solceansky@gmail.com>
2021-09-03 02:03:52 +0900

    take_footer_test コンパイルエラーのみ修正

--- test/UnitTest/PDFParserTest/take_footer_test.cpp
+++ test/UnitTest/PDFParserTest/take_footer_test.cpp
@@ -1,11 +1,11 @@
-#include "PDFParser.h"
 #include "literal_trim.hpp"
+#include "pdfparser.object_pool.hpp"
+#include "pdfparser.stream_parser.hpp"
 #include "take_footer_test.hpp"
 
 #include <sstream>
 
 using namespace pdfparser;
-using namespace error_types;
 using namespace object_types;
 using namespace pdfparser_test;
 
@@ -77,95 +77,6 @@ startxref
 	Assert::IsTrue(object_types::dictionary_object{{"Size", 1}} ==
 	               str_parser.take_footer(obj_pool));
 }
-void take_footer_test::test_EOF_EOF_not_EOL() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xref
-0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-startxref
-0
-%%EOF )";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const error_types::parse_error& parse_e) {
-		Assert::IsTrue(parse_error::EOL_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_footer_test::test_EOF_not_EOF() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xref
-0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-startxref
-0
-%EOF
-)";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const error_types::parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_EOF_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_footer_test::test_EOF_SP_EOF() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xref
-0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-startxref
-0
- %%EOF
-)";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const error_types::parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_EOF_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
 
 void take_footer_test::test_xref_byte_offset_not_found() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
@@ -179,8 +90,9 @@ void take_footer_test::test_xref_byte_offset_not_found() {
 
 	try {
 		str_parser.take_footer(obj_pool);
-	} catch (const error_types::parse_error& parse_e) {
-		Assert::IsTrue(parse_error::xref_byte_offset_not_found == parse_e.code());
+	} catch (const object_not_found_error& obj_e) {
+		Assert::IsTrue(object_not_found_error::integer_object_not_found ==
+		               obj_e.code());
 
 		// success
 		return;
@@ -215,66 +127,6 @@ trailer
 	Assert::IsTrue(object_types::dictionary_object{{"Size", 1}} ==
 	               str_parser.take_footer(obj_pool));
 }
-void take_footer_test::test_startxref_startxref_not_EOL() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xref
-0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-startxref 0
-%%EOF
-)";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::xref_byte_offset_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_footer_test::test_startxref_not_startxref() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xref
-0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-sstartxref
-0
-%%EOF
-)";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_startxref_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-
 void take_footer_test::test_xref_SP_xref_comment_EOL() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
 	                         std::ios_base::binary);
@@ -299,63 +151,4 @@ startxref
 
 	Assert::IsTrue(object_types::dictionary_object{{"Size", 1}} ==
 	               str_parser.take_footer(obj_pool));
-}
-void take_footer_test::test_xref_xref_not_EOL() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xref 0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-startxref
-0
-%%EOF
-)";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::EOL_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void take_footer_test::test_xref_not_xref() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-xxref
-0 1
-0000000000 65535 f 
-)"_trimmed;
-	stream << R"(
-trailer
-	<< /Size 1 >>
-startxref
-0
-%%EOF
-)";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-
-	try {
-		str_parser.take_footer(obj_pool);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_xref_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
 }
