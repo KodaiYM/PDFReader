93fdcbcf6bf222a6d454fe35f086451b7204339e
KodaiYM <solceansky@gmail.com>
2021-09-02 23:31:15 +0900

    take_stream_object_test の構文上の修正

--- test/UnitTest/PDFParserTest/take_stream_object_test.cpp
+++ test/UnitTest/PDFParserTest/take_stream_object_test.cpp
@@ -1,17 +1,15 @@
-#include "PDFParser.h"
 #include "literal_trim.hpp"
-#include "take_any_object_test@stream_object_test.hpp"
+#include "pdfparser.object_pool.hpp"
+#include "pdfparser.stream_parser.hpp"
+#include "take_stream_object_test.hpp"
 
 #include <sstream>
 
 using namespace pdfparser;
-using namespace error_types;
 using namespace object_types;
 using namespace pdfparser_test;
 
-using namespace take_any_object_test;
-
-void stream_object_test::test_sample_CRLF() {
+void take_stream_object_test::test_sample_CRLF() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
 	                         std::ios_base::binary);
 
@@ -22,12 +20,11 @@ void stream_object_test::test_sample_CRLF() {
 
 	stream_parser str_parser(std::move(stream));
 	object_pool   obj_pool(str_parser);
-	auto          object = str_parser.take_any_object(obj_pool);
-	Assert::IsTrue(
-	    stream_object{dictionary_object{{"Length", 18}}, " stream contents \n"} ==
-	    std::get<stream_object>(object));
+	auto          object = str_parser.take_stream_object(obj_pool);
+	Assert::IsTrue(stream_object{dictionary_object{{"Length", 18}},
+	                             " stream contents \n"} == object);
 }
-void stream_object_test::test_sample_LF() {
+void take_stream_object_test::test_sample_LF() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
 	                         std::ios_base::binary);
 
@@ -38,12 +35,11 @@ void stream_object_test::test_sample_LF() {
 
 	stream_parser str_parser(std::move(stream));
 	object_pool   obj_pool(str_parser);
-	auto          object = str_parser.take_any_object(obj_pool);
-	Assert::IsTrue(
-	    stream_object{dictionary_object{{"Length", 18}}, " stream contents \n"} ==
-	    std::get<stream_object>(object));
+	auto          object = str_parser.take_stream_object(obj_pool);
+	Assert::IsTrue(stream_object{dictionary_object{{"Length", 18}},
+	                             " stream contents \n"} == object);
 }
-void stream_object_test::test_indirect_reference_Length() {
+void take_stream_object_test::test_indirect_reference_Length() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
 	                         std::ios_base::binary);
 
@@ -70,33 +66,12 @@ endobj
 	obj_pool.add_xref_table(
 	    xref_types::xref_table{xref_types::xref_inuse_entry{1, 0, 0}});
 
-	auto object = str_parser.take_any_object(obj_pool);
+	auto object = str_parser.take_stream_object(obj_pool);
 	Assert::IsTrue(
 	    stream_object{dictionary_object{{"Length", indirect_reference{1, 0}}},
-	                  " stream contents "} == std::get<stream_object>(object));
+	                  " stream contents "} == object);
 }
-void stream_object_test::test_stream_CR() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << "<</Length 18>>\n"
-	       << "stream\r"
-	       << " stream contents \n\n"
-	       << "endstream";
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-	try {
-		str_parser.take_any_object(obj_pool);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::stream_CRLF_or_LF_not_found == parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void stream_object_test::test_absence_of_Length_entry() {
+void take_stream_object_test::test_absence_of_Length_entry() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
 	                         std::ios_base::binary);
 
@@ -110,7 +85,7 @@ endstream
 	stream_parser str_parser(std::move(stream));
 	object_pool   obj_pool(str_parser);
 	try {
-		str_parser.take_any_object(obj_pool);
+		str_parser.take_stream_object(obj_pool);
 	} catch (const parse_error& parse_e) {
 		Assert::IsTrue(parse_error::stream_dictionary_absence_of_Length_entry ==
 		               parse_e.code());
@@ -120,7 +95,7 @@ endstream
 	}
 	Assert::Fail();
 }
-void stream_object_test::test_data_is_shorter_than_Length() {
+void take_stream_object_test::test_data_is_shorter_than_Length() {
 	std::stringstream stream(std::ios_base::in | std::ios_base::out |
 	                         std::ios_base::binary);
 
@@ -134,33 +109,10 @@ endstream
 	stream_parser str_parser(std::move(stream));
 	object_pool   obj_pool(str_parser);
 	try {
-		str_parser.take_any_object(obj_pool);
+		str_parser.take_stream_object(obj_pool);
 	} catch (const parse_error& parse_e) {
 		Assert::IsTrue(parse_error::stream_data_is_shorter_than_Length ==
 		               parse_e.code());
-
-		// success
-		return;
-	}
-	Assert::Fail();
-}
-void stream_object_test::test_keyword_endstream_not_found() {
-	std::stringstream stream(std::ios_base::in | std::ios_base::out |
-	                         std::ios_base::binary);
-
-	stream << R"(
-<</Length 16>>
-stream
- stream contents
-endstreeam
-)"_trimmed;
-
-	stream_parser str_parser(std::move(stream));
-	object_pool   obj_pool(str_parser);
-	try {
-		str_parser.take_any_object(obj_pool);
-	} catch (const parse_error& parse_e) {
-		Assert::IsTrue(parse_error::keyword_endstream_not_found == parse_e.code());
 
 		// success
 		return;
---
+++ test/UnitTest/PDFParserTest/take_stream_object_test.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "testtool.h"
+
+namespace pdfparser_test {
+[TestClass] public ref class take_stream_object_test {
+public:
+	[TestMethod] void test_sample_CRLF();
+	[TestMethod] void test_sample_LF();
+	[TestMethod] void test_indirect_reference_Length();
+	[TestMethod] void test_absence_of_Length_entry();
+	[TestMethod] void test_data_is_shorter_than_Length();
+};
+} // namespace pdfparser_test
