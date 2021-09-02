47bef13bf1c426de4cb13d95c1f96cd317eb325b
KodaiYM <solceansky@gmail.com>
2021-09-03 01:08:00 +0900

    take_object_test の作成と、object_not_found_error の catch 時にエラーを特定するようにした

---
+++ test/UnitTest/PDFParserTest/take_object_test.cpp
@@ -0,0 +1,167 @@
+#include "literal_trim.hpp"
+#include "pdfparser.object_pool.hpp"
+#include "pdfparser.stream_parser.hpp"
+#include "take_object_test.hpp"
+
+#include <sstream>
+
+using namespace pdfparser;
+using namespace object_types;
+using namespace pdfparser_test;
+
+void take_object_test::test_any_direct_object_boolean() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "true";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(true == std::get<boolean_object>(object));
+}
+void take_object_test::test_any_direct_object_integer() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "10";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(10 == std::get<integer_object>(object));
+}
+void take_object_test::test_any_direct_object_real() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "1.2";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(1.2 == std::get<real_object>(object));
+}
+void take_object_test::test_any_direct_object_string() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "(str)";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue("str" == std::get<string_object>(object));
+}
+void take_object_test::test_any_direct_object_name() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "/name";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue("name" == std::get<name_object>(object));
+}
+void take_object_test::test_any_direct_object_array() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "[(s) 1 0.4]";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(array_object{string_object("s"), 1, 0.4} ==
+	               std::get<array_object>(object));
+}
+void take_object_test::test_any_direct_object_dictionary() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "<</key(value)>>";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(dictionary_object{{"key", string_object("value")}} ==
+	               std::get<dictionary_object>(object));
+}
+void take_object_test::test_any_direct_object_stream() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << R"(
+<</Length 9>>
+stream
+123456789
+endstream
+)"_trimmed;
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue("123456789" ==
+	               std::get<stream_object>(object).get_decoded_data());
+}
+void take_object_test::test_any_direct_object_null() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "null";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(null == std::get<null_object>(object));
+}
+void take_object_test::test_any_direct_object_indirect_reference() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "1 2 R";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	auto object = str_parser.take_object<any_direct_object_or_ref>(obj_pool);
+	Assert::IsTrue(indirect_reference{1, 2} ==
+	               std::get<indirect_reference>(object));
+}
+
+void take_object_test::test_no_object() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream << "nothing";
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	try {
+		str_parser.take_object<any_direct_object>(obj_pool);
+	} catch (const object_not_found_error& obj_e) {
+		Assert::IsTrue(object_not_found_error::specified_object_not_found ==
+		               obj_e.code());
+
+		// success
+		return;
+	}
+	Assert::Fail();
+}
+void take_object_test::test_eof() {
+	std::stringstream stream(std::ios_base::in | std::ios_base::out |
+	                         std::ios_base::binary);
+
+	stream_parser str_parser(std::move(stream));
+	object_pool   obj_pool(str_parser);
+	try {
+		str_parser.take_object<any_direct_object>(obj_pool);
+	} catch (const object_not_found_error& obj_e) {
+		Assert::IsTrue(object_not_found_error::specified_object_not_found ==
+		               obj_e.code());
+
+		// success
+		return;
+	}
+	Assert::Fail();
+}
---
+++ test/UnitTest/PDFParserTest/take_object_test.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "testtool.h"
+
+namespace pdfparser_test {
+[TestClass] public ref class take_object_test {
+public:
+	[TestMethod] void test_any_direct_object_boolean();
+	[TestMethod] void test_any_direct_object_integer();
+	[TestMethod] void test_any_direct_object_real();
+	[TestMethod] void test_any_direct_object_string();
+	[TestMethod] void test_any_direct_object_name();
+	[TestMethod] void test_any_direct_object_array();
+	[TestMethod] void test_any_direct_object_dictionary();
+	[TestMethod] void test_any_direct_object_stream();
+	[TestMethod] void test_any_direct_object_null();
+	[TestMethod] void test_any_direct_object_indirect_reference();
+
+	[TestMethod] void test_no_object();
+	[TestMethod] void test_eof();
+};
+} // namespace pdfparser_test
