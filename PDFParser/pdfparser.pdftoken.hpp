26f4385ee0696a8fb410b546aeba7ae016e1340d
KodaiYM <solceansky@gmail.com>
2021-09-03 13:26:24 +0900

    pdftoken の実装

---
+++ PDFParser/pdfparser.pdftoken.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace pdfparser {
+struct pdftoken {
+	enum token_type { delimiter_token, regular_token };
+
+public:
+	/// <returns>this token type</returns>
+	[[nodiscard]] token_type type() const noexcept;
+
+	/// <returns>this token string</returns>
+	[[nodiscard]] std::string str() const;
+	                          operator std::string() const;
+
+	/// <returns>view of this token string</returns>
+	[[nodiscard]] std::string_view sview() const noexcept;
+	                               operator std::string_view() const noexcept;
+
+public:
+	pdftoken(token_type type, std::string token_str) noexcept;
+
+private:
+	/// <summary>token type of this token</summary>
+	token_type m_type;
+
+	/// <summary>token string</summary>
+	std::string m_token_str;
+};
+} // namespace pdfparser
