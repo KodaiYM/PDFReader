cebcc749f79abd5d18072157c263b1650e0d9dde
KodaiYM <solceansky@gmail.com>
2021-09-19 17:24:33 +0900

    不要なコードの削除

--- PDFReaderModel/PagesViewModel.fs
+++ PDFReaderModel/PagesViewModel.fs
@@ -6,9 +6,6 @@ open System.Collections.ObjectModel
 open System.Diagnostics
 
 type PagesViewModel() =
-   //   let mutable _Text = "Hello, PDF Parser Test Program!"
-   //   let PropertyChanged = Event<_, _>()
-
    member val Pages = ObservableCollection<PDFParser.PDFPage>()
 
    member this.LoadPages() =
@@ -27,18 +24,4 @@ type PagesViewModel() =
                Height = this.ToDefaultUserSpace page.Height
             )
 
-      this.Pages.Add(PDFParser.PDFPage(Width = 200.0, Height = 400.0))
-      this.Pages.Add(PDFParser.PDFPage(Width = 595.0, Height = 842.0))
-
    member this.ToDefaultUserSpace point = point / 72.0 * 96.0
-(*
-   interface INotifyPropertyChanged with
-      [<CLIEventAttribute>]
-      member this.PropertyChanged = PropertyChanged.Publish
-
-   member this.Text
-      with get () = _Text
-      and set (newText) =
-         _Text <- newText
-         PropertyChanged.Trigger(this, PropertyChangedEventArgs("Text"))
-         *)
